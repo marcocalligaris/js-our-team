c4ffbe31d3d39171305e91a30b826285d561012c
marcocalligaris <marcocally@gmail.com>
2022-07-05 15:29:08 +0200

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@
 // # MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
+//* Milestone 0
+
+const teamMembers = [
+    {firstName: 'Wayne', lastName: 'Barnett', role: 'Founder & CEO', photo: 'wayne-barnett-founder-ceo.jpg',},
+    {firstName: 'Angela', lastName: 'Caroll', role: 'Chief Editor', photo: 'angela-caroll-chief-editor.jpg',},
+    {firstName: 'Walter', lastName: 'Gordon', role: 'Office Manager', photo: 'walter-gordon-office-manager.jpg',},
+    {firstName: 'Angela', lastName: 'Lopez', role: 'Social Media Manager', photo: 'angela-lopez-social-media-manager.jpg',},
+    {firstName: 'Scott', lastName: 'Estrada', role: 'Developer', photo: 'scott-estrada-developer.jpg',},
+    {firstName: 'Barbara', lastName: 'Ramos', role: 'Graphic Designer', photo: 'barbara-ramos-graphic-designer.jpg ',},
+]
+
+console.log(teamMembers);
